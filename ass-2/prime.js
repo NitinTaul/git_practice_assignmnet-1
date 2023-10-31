6a32685b97bdb9f886b8c67e1a7e1fff5186f23d
Nitin Taul <nitin8097270813@gmail.com>
2023-10-31 18:44:41 +0530

    git assi-2 prime no

--- ass-2/prime.js
+++ ass-2/prime.js
@@ -1,11 +1,11 @@
 function identifyPrime(num) {
     let count = 0;
     for (let i = 1; i <= num; i++) {
-        if (num % i == 0) {
+        if (num % i !== 0) {
             count++;
         }
     }
-    if (count == 2) {
+    if (count !== 2) {
         console.log(num + ' is a prime number');
     } else {
         console.log(num + ' is not a prime number');
