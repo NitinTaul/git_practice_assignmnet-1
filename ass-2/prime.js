ef8cb26fa97c157457c6ca17ded510a936d27292
suraj <surajlokhande7744@gmail.com>
2023-10-31 19:24:48 +0530

    prime number

--- ass-2/prime.js
+++ ass-2/prime.js
@@ -1,17 +1,16 @@
 function identifyPrime(num) {
-    let count = 0;
-    for (let i = 1; i <= num; i++) {
-        if (num % i !== 0) {
-            count++;
-        }
-    }
-    if (count !== 2) {
-        console.log(num + ' is a prime number');
-    } else {
-        console.log(num + ' is not a prime number');
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i !== 0) {
+      count++;
     }
   }
-  
-  const num = 17;
-  identifyPrime(num);
-  
+  if (count !== 2) {
+    console.log(num + " is a prime number");
+  } else {
+    console.log(num + " is not a prime number");
+  }
+}
+
+const num = 17;
+identifyPrime(num);
